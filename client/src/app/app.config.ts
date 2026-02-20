cc32bd93ebf7cbe82441db4bcdfc89cfa91d7be0
Nehir <nehircelikel@gmail.com>
2026-02-20 23:39:28 +0300

    section 10

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import 'zone.js';
-
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
