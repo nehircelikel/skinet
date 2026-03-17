3d68e8da1eb6fbe0d21094863778b33b20b8806b
Nehir <nehircelikel@gmail.com>
2026-03-17 18:25:02 +0300

    section 14

--- API/Program.cs
+++ API/Program.cs
@@ -27,16 +27,21 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
     return ConnectionMultiplexer.Connect(configuration);
 });
 builder.Services.AddSingleton<ICartService, CartService>();
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<AppUser>()
+    .AddEntityFrameworkStores<StoreContext>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
 .WithOrigins("http://localhost:4200","https://localhost:4200"));
 
 
 app.MapControllers();
+
+app.MapGroup("api").MapIdentityApi<AppUser>(); //api/login
 
 try
 {
